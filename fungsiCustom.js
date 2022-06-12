cd6a9242ba349910f2706c62dd42f5e039f77e8c
muttaqinyoga <muttaqinyoga@gmail.com>
2022-06-12 23:38:10 +0700

    Menyelesaikan tugas sesuai dengan yang diberikan

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require('fs');
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,42 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = callback => {
+  let results = [];
+  fs.readFile(
+    file1, { encoding: "utf-8" },
+    (err, res) => {
+      if(err){
+        return callback(err, null);
+      }
+      const data = JSON.parse(res);
+      results.push(data.message.split(" ")[1]);
+    }
+  );
+
+  fs.readFile(
+    file2, { encoding: "utf-8" },
+    (err, res) => {
+      if(err){
+        return callback(err, null);
+      }
+      const data2 = JSON.parse(res);
+      results.push(data2[0].message.split(" ")[1]);
+    }
+  );
+
+  fs.readFile(
+    file3, { encoding: "utf-8" },
+    (err, res) => {
+      if(err){
+        return callback(err, null);
+      }
+      const data3 = JSON.parse(res);
+      results.push(data3[0].data.message.split(" ")[1]);
+      callback(null, results);
+    }
+  )
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
